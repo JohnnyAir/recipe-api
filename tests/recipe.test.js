b9625a80817f3f21751ca6fed84aa15631cf8666
john <tobiz2002@gmail.com>
2019-11-24 14:16:03 +0100

    test get recipe

--- tests/recipe.test.js
+++ tests/recipe.test.js
@@ -2,33 +2,19 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../server");
 
-// const base_url = "localhost:3000";
 const api_url = "/api/recipes";
 
 const { expect } = chai;
-// let recipeId;
+let recipeId;
 
 chai.use(chaiHttp);
 
 describe("Recipe", () => {
   describe("create recipe", () => {
-    it("it should get all recipe", done => {
-      chai
-        .request(app)
-        .get(api_url)
-        .end((err, response) => {
-          if (err) {
-            console.warn(err);
-          }
-          expect(response).to.have.status(200);
-          done();
-        });
-    });
     it("it should create recipe", done => {
       chai
         .request(app)
         .post(api_url)
-        .set("Content-Type", "application/x-www-form-urlencoded")
         .send({
           title: "Rice Stew",
           ingredients: "pepper, seasoning, ",
@@ -48,7 +34,41 @@ describe("Recipe", () => {
           expect(res.data).to.have.property("instructions");
           expect(res.data).to.have.property("time");
           expect(res.data).to.have.property("difficulty");
-          recipeId = res.data ? res.data.id : "";
+          recipeId = res.data ? res.data._id : "";
+          done();
+        });
+    });
+  });
+
+  describe("get recipe", () => {
+    it("it should get all recipe", done => {
+      chai
+        .request(app)
+        .get(api_url)
+        .end((err, response) => {
+          if (err) {
+            console.warn(err);
+          }
+          expect(response).to.have.status(200);
+          done();
+        });
+    });
+
+    it("it should get a recipe", done => {
+      chai
+        .request(app)
+        .get(`${api_url}/${recipeId}`)
+        .end((err, response) => {
+          if (err) {
+            console.warn(err);
+          }
+          const res = JSON.parse(response.text);
+          expect(response).to.have.status(200);
+          expect(res).to.have.property("title");
+          expect(res).to.have.property("ingredients");
+          expect(res).to.have.property("instructions");
+          expect(res).to.have.property("time");
+          expect(res).to.have.property("difficulty");
           done();
         });
     });
