39800a5b4e94ea388d3596f82e5576c80a1f1f69
john <tobiz2002@gmail.com>
2019-12-04 11:33:04 +0100

    added travis yml

--- tests/recipe.test.js
+++ tests/recipe.test.js
@@ -73,4 +73,45 @@ describe("Recipe", () => {
         });
     });
   });
+
+  describe("modify recipe", () => {
+    it("it should change recipe info", done => {
+      chai
+        .request(app)
+        .post(`${api_url}/${recipeId}`)
+        .send({
+          title: "Rice Stew",
+          ingredients: "pepper, seasoning, ",
+          instructions: "fry pepper",
+          time: 2,
+          difficulty: 5
+        })
+        .end((err, response) => {
+          if (err) {
+            console.warn(err);
+          }
+          expect(response).to.have.status(200);
+          done();
+        });
+    });
+
+    it("it should get a recipe", done => {
+      chai
+        .request(app)
+        .get(`${api_url}/${recipeId}`)
+        .end((err, response) => {
+          if (err) {
+            console.warn(err);
+          }
+          const res = JSON.parse(response.text);
+          expect(response).to.have.status(200);
+          expect(res).to.have.property("title");
+          expect(res).to.have.property("ingredients");
+          expect(res).to.have.property("instructions");
+          expect(res).to.have.property("time");
+          expect(res).to.have.property("difficulty");
+          done();
+        });
+    });
+  });
 });
